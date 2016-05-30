60538c505570e86f445c8d0f24b7c6870adbc135
Daniil Smirnov <danslapman@gmail.com>
2016-05-31 01:21:25 +0300

    Re-arrange calls

--- src/store.rs
+++ src/store.rs
@@ -6,7 +6,6 @@ use domain::RecordCell;
 use bincode::SizeLimit;
 use bincode::rustc_serialize::{encode, decode};
 
-#[derive(Clone)]
 pub struct Store {
     pub path: String
 }
@@ -14,23 +13,23 @@ pub struct Store {
 impl Store {
     fn read_all(&self) -> Vec<RecordCell> {
         let mut contents = Vec::<u8>::new();
-        let _ = File::open(self.clone().path).map(|mut f| f.read_to_end(&mut contents));
+        let _ = File::open(self.path.clone()).map(|mut f| f.read_to_end(&mut contents));
         let entries: Vec<RecordCell> = decode(&contents[..]).unwrap_or(Vec::<RecordCell>::new());
         entries
     }
     
     pub fn persist(&self, entry: RecordCell) -> () {
-        let mut entries = self.clone().read_all();
+        let mut entries = self.read_all();
         entries.push(entry);
         
-        let _ = File::create(self.clone().path).unwrap()
+        let _ = File::create(self.path.clone()).unwrap()
             .write(encode(&entries, SizeLimit::Infinite).unwrap().as_slice()).unwrap();
         ()
     }
     
     pub fn read(&self, domain: String) -> Option<RecordCell> {
         let mut contents = Vec::<u8>::new();
-        let _ = File::open(self.clone().path).unwrap().read_to_end(&mut contents);
+        let _ = File::open(self.path.clone()).unwrap().read_to_end(&mut contents);
         let entries: Vec<RecordCell> = decode(&contents[..]).unwrap();
         entries.into_iter().fold(None, move |acc, el| if el.domain == domain { Some(el) } else { acc })
     }
